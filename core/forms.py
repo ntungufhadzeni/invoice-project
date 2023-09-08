e9f7c2edfb7792003407c76b6410f770ca6c0bb4
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2023-09-08 21:09:25 +0200

    add more models

--- core/forms.py
+++ core/forms.py
@@ -8,7 +8,17 @@ from .models import Company
 class CompanyForm(forms.ModelForm):
     class Meta:
         model = Company
-        fields = ['name', 'logo', 'billing_address', 'contact_number', 'email', 'currency']
+        fields = ['name',
+                  'logo',
+                  'billing_address',
+                  'bank_name',
+                  'account_number',
+                  'branch_name',
+                  'branch_code',
+                  'branch_code_electronic',
+                  'contact_number',
+                  'email',
+                  'currency']
 
     currency = forms.ChoiceField(choices=Company.CURRENCY_CHOICES)
 
--- core/models.py
+++ core/models.py
@@ -27,20 +27,61 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Company(models.Model):
     CURRENCY_CHOICES = (
+        ('ZAR', 'South African Rand'),
         ('USD', 'US Dollar'),
         ('EUR', 'Euro'),
         ('GBP', 'British Pound'),
-        ('ZAR', 'South African Rand'),
         # Add other currency choices as needed
     )
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    logo = models.ImageField(upload_to='company_logos/', null=True, blank=True)
-    billing_address = models.TextField(null=True, blank=True)
-    contact_number = models.CharField(max_length=20, null=True, blank=True)
-    email = models.EmailField(null=True, blank=True)
-    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default='USD')
+    logo = models.ImageField(upload_to='company_logos/')
+    color = models.CharField(blank=True, null=True)
+    billing_address = models.TextField()
+    bank_name = models.CharField(max_length=30)
+    account_number = models.CharField(max_length=20)
+    branch_name = models.CharField(max_length=30)
+    branch_code = models.CharField(max_length=10)
+    branch_code_electronic = models.CharField(max_length=10, blank=True, null=True, verbose_name="Branch code (electronic)")
+    contact_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default='ZAR')
 
     def __str__(self):
         return self.name
+
+
+class Invoice(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    customer = models.CharField(max_length=100)
+    customer_email = models.EmailField(null=True, blank=True)
+    billing_address = models.TextField(null=True, blank=True)
+    date = models.DateField()
+    due_date = models.DateField(null=True, blank=True)
+    message = models.TextField(default="this is a default message.")
+    total_amount = models.DecimalField(max_digits=9, decimal_places=2, blank=True, null=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.customer)
+
+    def get_status(self):
+        return self.status
+
+    # def save(self, *args, **kwargs):
+    # if not self.id:
+    #     self.due_date = datetime.datetime.now()+ datetime.timedelta(days=15)
+    # return super(Invoice, self).save(*args, **kwargs)
+
+
+class LineItem(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    service = models.TextField()
+    description = models.TextField()
+    quantity = models.IntegerField()
+    rate = models.DecimalField(max_digits=9, decimal_places=2)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return str(self.customer)

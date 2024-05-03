de45cf165ce6c8d058b6da56a0b8a839fa81857b
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2024-05-03 23:02:17 +0200

    remove urls

--- invoice_project/urls.py
+++ invoice_project/urls.py
@@ -10,5 +10,3 @@ urlpatterns = [
     path('', include('companies.urls')),
     path('accounts/', include('users.urls'))
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
